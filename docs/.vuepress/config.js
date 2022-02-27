49d8b9f74c82a632150bf83ce1041d2cce0afa34
chamberlain99 <7580525+chamberlain99@user.noreply.gitee.com>
2022-02-27 17:55:39 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'TypeScript4 文档',
+    description: 'TypeScript4 最新官方文档翻译',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '张伯伦 JavaScript 博客', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/chamberlain99' },
+                    { text: '掘金', link: 'https://juejin.cn/user/1855631360537159/posts' }
+                ]
+            }
+        ]
+    }
+  }
+  

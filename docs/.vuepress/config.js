7c32dac9163e6cc7b2ac80743eb99ae946adcd42
chenchucheng <chenchucheng@zeen-cn.com>
2022-06-13 17:40:46 +0800

    打包

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
           lang: 'zh-CN'
         }
     },
-    base:'/', //https://<USERNAME>.github.io/
+    base:'/blog/', //https://<USERNAME>.github.io/
     head:[
       ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标) 
     ],
@@ -37,47 +37,47 @@ module.exports = {
         },
     
         // 博主信息 (显示在首页侧边栏)
-        // blogger: {
-        //   avatar: 'https://fastly.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-        //   name: 'chamberlain',
-        //   slogan: '前端持续学习者',
-        // },
+        blogger: {
+          avatar: 'https://fastly.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
+          name: 'chamberlain',
+          slogan: '前端持续学习者',
+        },
     
         // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
-        // social: {
-        //   // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
-        //   icons: [
-        //     {
-        //       iconClass: 'icon-youjian',
-        //       title: '发邮件',
-        //       link: 'chamberlain_mail@163.com',
-        //     },
-        //     {
-        //       iconClass: 'icon-github',
-        //       title: 'GitHub',
-        //       link: 'https://github.com/chamberlain99',
-        //     },
-        //     {
-        //       iconClass: 'icon-erji',
-        //       title: '听音乐',
-        //       link: 'https://music.163.com/#/playlist?id=755597173',
-        //     },
-        //   ],
-        // },
+        social: {
+          // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
+          icons: [
+            {
+              iconClass: 'icon-youjian',
+              title: '发邮件',
+              link: 'chamberlain_mail@163.com',
+            },
+            {
+              iconClass: 'icon-github',
+              title: 'GitHub',
+              link: 'https://github.com/chamberlain99',
+            },
+            {
+              iconClass: 'icon-erji',
+              title: '听音乐',
+              link: 'https://music.163.com/#/playlist?id=755597173',
+            },
+          ],
+        },
     
         // 页脚信息
-        // footer: {
-        //   createYear: 2019, // 博客创建年份
-        //   copyrightInfo:
-        //     'chamberlain | <a href="https://github.com/Chamberlain99/blog/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
-        // },
-        // vssueConfig: {
-        //   platform: 'github',
-        //   owner: 'Chamberlain99',
-        //   repo: 'vue-press-blog-system',
-        //   clientId: 'f3885593c58b486b414b',
-        //   clientSecret: 'f854e5b4f2a93b80c947e3b79257a77aab2c33dd',
-        // }
+        footer: {
+          createYear: 2019, // 博客创建年份
+          copyrightInfo:
+            'chamberlain | <a href="https://github.com/Chamberlain99/blog/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签或换行标签</br>
+        },
+        vssueConfig: {
+          platform: 'github',
+          owner: 'Chamberlain99',
+          repo: 'vue-press-blog-system',
+          clientId: 'f3885593c58b486b414b',
+          clientSecret: 'f854e5b4f2a93b80c947e3b79257a77aab2c33dd',
+        }
     },
     markdown: {
       lineNumbers: true,

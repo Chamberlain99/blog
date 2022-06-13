4475a628608732b2ad8a385b635dcd86630c8f12
chenchucheng <chenchucheng@zeen-cn.com>
2022-06-13 15:10:36 +0800

    test:测试自动部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -101,58 +101,6 @@ module.exports = {
           },
         },
       ],
-      // [
-      //   'vuepress-plugin-comment', // 评论
-      //   {
-      //     choosen: 'gitalk',
-      //     options: {
-      //       clientID: 'a6e1355287947096b88b',
-      //       clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-      //       repo: 'blog', // GitHub 仓库
-      //       owner: 'Chamberlain99', // GitHub仓库所有者
-      //       admin: ['Chamberlain99'], // 对仓库有写权限的人
-      //       // distractionFreeMode: true,
-      //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
-      //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-      //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-      //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-      //       body:
-      //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-      //     },
-      //   },
-      // ],
-
-    // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
-    // [
-    //   'thirdparty-search',
-    //   {
-    //     thirdparty: [
-    //       {
-    //         title: '在MDN中搜索',
-    //         frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-    //         behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-    //       },
-    //       {
-    //         title: '在Runoob中搜索',
-    //         frontUrl: 'https://www.runoob.com/?s=',
-    //       },
-    //       {
-    //         title: '在Vue API中搜索',
-    //         frontUrl: 'https://cn.vuejs.org/v2/api/#',
-    //       },
-    //       {
-    //         title: '在Bing中搜索',
-    //         frontUrl: 'https://cn.bing.com/search?q=',
-    //       },
-    //       {
-    //         title: '通过百度搜索本站的',
-    //         frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-    //       },
-    //     ],
-    //   }
-    // ],
-
-
      ]
   }
   

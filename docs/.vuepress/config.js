12862f7c1a762a66d72fcd2a7cf2d23e362409ba
chamberlain <chamberlain_mail@163.com>
2022-07-20 22:57:00 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const sidebar = require('./sidebar.js')
 const nav = require('./nav.js')
 /*   vding vuepress博客主题插件文档地址：https://doc.xugaoyi.com/  */
 module.exports = {
-    title: '程序员成长-修炼中心 「作者：陈楚城同学」',
+    title: '程序员成长-修炼中心 「作者：陈楚城」',
     description: '个人博客-学习总结',
     theme:'vdoing',     
     locales: {
